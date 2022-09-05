724a4c64fba749ebeb3e9ac244b9b4b9ca0340bf
Vadym <turokvadim2510@gmail.com>
2022-09-06 01:01:19 +0300

    .

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,18 +1,18 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.F_API_KEY,
-    authDomain: process.env.F_AUTH_DOMAIN,
-    projectId: process.env.F_PROJECT_ID,
-    storageBucket: process.env.F_STORAGE_BUCKET,
-    messagingSenderId: process.env.F_MESSAGING_SENDER_ID,
-    appId: process.env.F_APP_ID
+    apiKey: "AIzaSyBR-Mjx8WqWFrP9Ea0YQf292yRA0yDSe80",
+    authDomain: "chat-application-deaf3.firebaseapp.com",
+    projectId: "chat-application-deaf3",
+    storageBucket: "chat-application-deaf3.appspot.com",
+    messagingSenderId: "884701103260",
+    appId: "1:884701103260:web:a11b50e42a4018d72c1959"
 };
 
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const firestore = getFirestore(app)
 
